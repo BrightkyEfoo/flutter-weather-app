b7514fe64ca8ebcf73c67029991f312509d9eb72
BrightkyEfoo <brightefoo@gmail.com>
2023-12-01 18:13:51 +0100

    Create and code weather service

---
+++ lib/services/weather_service.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:helloapp/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  // ignore: constant_identifier_names
+  static const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load weather');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    // fetch current location
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    // convert
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
